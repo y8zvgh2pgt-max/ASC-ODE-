2351ba0a652af55a07d4d9069b4a5124cd80b8f0
Maximilian Seiringer <maximilian.seiringer@tuwien.ac.at>
2025-11-24 11:41:46 +0100

    added timestepper changes

--- src/timestepper.hpp
+++ src/timestepper.hpp
@@ -56,7 +56,69 @@ namespace ASC_ode
   };
 
 
-  
+  class ImprovedEuler : public TimeStepper
+  {
+    Vector<> m_k1;
+    Vector<> m_k2;
+    Vector<> m_ytmp;
+
+  public:
+    ImprovedEuler(std::shared_ptr<NonlinearFunction> rhs) 
+    : TimeStepper(rhs), 
+      m_k1(rhs->dimF()), 
+      m_k2(rhs->dimF()), 
+      m_ytmp(rhs->dimX()) 
+    { }
+
+    void DoStep(double tau, VectorView<double> y) override
+    {
+      m_rhs->evaluate(y, m_k1);
+
+      m_ytmp = y;
+      m_ytmp += tau * m_k1;
+
+      m_rhs->evaluate(m_ytmp, m_k2);
+
+      y += (0.5 * tau) * m_k1;
+      y += (0.5 * tau) * m_k2;
+    }
+  };
+
+
+  class CrankNicolson : public TimeStepper
+  {
+    std::shared_ptr<NonlinearFunction> m_equ;
+    std::shared_ptr<Parameter> m_tau;
+    std::shared_ptr<ConstantFunction> m_yold;
+    std::shared_ptr<ConstantFunction> m_f_old;
+    Vector<> m_tmp_f;
+
+  public:
+    CrankNicolson(std::shared_ptr<NonlinearFunction> rhs) 
+    : TimeStepper(rhs),
+      m_tau(std::make_shared<Parameter>(0.0)),
+      m_yold(std::make_shared<ConstantFunction>(rhs->dimX())),
+      m_f_old(std::make_shared<ConstantFunction>(rhs->dimF())),
+      m_tmp_f(m_rhs->dimF())
+    {      
+      auto ynew = std::make_shared<IdentityFunction>(rhs->dimX());
+      auto half_tau_f_new = 0.5 * (m_tau * m_rhs);
+      auto half_tau_f_old = 0.5 * (m_tau * m_f_old);
+      
+      m_equ = ynew - m_yold - half_tau_f_old - half_tau_f_new;
+    }
+
+    void DoStep(double tau, VectorView<double> y) override
+    {
+      m_yold->set(y);
+      m_tau->set(tau);
+      
+      m_rhs->evaluate(y, m_tmp_f);
+      m_f_old->set(m_tmp_f);
+
+      NewtonSolver(m_equ,y);
+    }
+  };
 
 }
 
